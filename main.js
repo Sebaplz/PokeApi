314c277f40080b729415e182a61846e502598d40
Sebastián Neira <tz_seb@hotmail.com>
2022-07-21 20:14:43 -0400

    Master

---
+++ main.js
@@ -0,0 +1,46 @@
+const flex = document.querySelector(".flex");
+const template = document.getElementById("card").content;
+const clone = template.cloneNode(true);
+const fragment = document.createDocumentFragment();
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  let random = getRandomInt(1, 152);
+  fetchData(random);
+});
+
+const fetchData = async (id) => {
+  try {
+    console.log(id);
+
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const data = await res.json();
+
+    console.log(data);
+
+    const pokemon = {
+      img: data.sprites.front_default,
+      nombre: data.name,
+    };
+
+    pintarCard(pokemon);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const pintarCard = (pokemon) => {
+  clone.querySelector(".card-img-top").setAttribute("src", pokemon.img);
+  clone.querySelector(".card-title").innerHTML = `${pokemon.nombre}`;
+
+  fragment.appendChild(clone);
+  flex.appendChild(fragment);
+};
+
+function btnRandom() {
+  let random = getRandomInt(1, 152);
+  fetchData(random);
+}
